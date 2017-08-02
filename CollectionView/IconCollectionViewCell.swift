70dd126fb86f91fe9f60b2cc70a8aab06f2d4aec
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-02 11:35:02 +0300

    Add custom classes and Outlets

---
+++ CollectionView/IconCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  IconCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Fomin Mykola on 8/2/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var iconPriceLabel: UILabel!
+}
