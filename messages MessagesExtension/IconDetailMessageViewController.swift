3e8b6a947ae553cfba322b6df30e52c091ba0983
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-07 13:20:51 +0300

    Added IconDetailMessageViewController

--- messages MessagesExtension/IconDetailMessageViewController.swift
+++ messages MessagesExtension/IconDetailMessageViewController.swift
@@ -1,15 +1,15 @@
 //
-//  IconDetailViewController.swift
-//  CollectionView
+//  IconDetailMessageViewController.swift
+//  messages MessagesExtension
 //
-//  Created by Fomin Mykola on 8/2/17.
+//  Created by Fomin Mykola on 8/7/17.
 //  Copyright © 2017 Fomin Mykola. All rights reserved.
 //
 
 import UIKit
 import IconDataKit
 
-class IconDetailViewController: UIViewController {
+class IconDetailMessageViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel! {
         didSet {
@@ -29,16 +29,17 @@ class IconDetailViewController: UIViewController {
     @IBOutlet weak var priceLabel: UILabel! {
         didSet {
             if let icon = icon {
-                priceLAbel.text = "$\(icon.price)"
+                priceLabel.text = "$\(icon.price)"
             }
         }
     }
     
     var icon: Icon?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
+
 
 }
