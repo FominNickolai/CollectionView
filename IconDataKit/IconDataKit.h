e1a871927edb2d4282cc29d2cd861cb8d165ebfa
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-07 13:06:26 +0300

    no message

---
+++ IconDataKit/IconDataKit.h
@@ -0,0 +1,19 @@
+//
+//  IconDataKit.h
+//  IconDataKit
+//
+//  Created by Fomin Mykola on 8/7/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IconDataKit.
+FOUNDATION_EXPORT double IconDataKitVersionNumber;
+
+//! Project version string for IconDataKit.
+FOUNDATION_EXPORT const unsigned char IconDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IconDataKit/PublicHeader.h>
+
+
